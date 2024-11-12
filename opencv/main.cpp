348c3d4ccb680f4b1afe4a9f55eab650c65dd90d
Petras Vestartas <petrasvestartas@gmail.com>
2024-11-12 23:43:36 +0100

    ADD c_cpp_properties for opencv

--- opencv/main.cpp
+++ opencv/main.cpp
@@ -4,9 +4,7 @@
 // https://docs.opencv.org/4.x/d9/df8/tutorial_root.html
 
 int main(){
-    std::string imagePath = "C:/brg/2_code/nest/opencv/image.jpg";
-    cv::Mat image = cv::imread(imagePath, cv::IMREAD_COLOR);
-    cv::imshow("Display window", image);
+    Mat
     cv::waitKey(0);
     return 0;
 }
